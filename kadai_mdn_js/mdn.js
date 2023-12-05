7286db899c0aee9cc2c8034fddafcdc597b129ca
mizuki-boop <149584819+mizuki-boop@users.noreply.github.com>
2023-12-05 13:39:51 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,4 +1,6 @@
-const today = new Date();
+let today = new Date();
+let year=today.getFullYear()+"年";
+let month=today.getMonth()+1+"月";
+let day=today.getDate()+"日";
 
-
-console.log(today.getFullYear()+"年"+today.getMonth()+"月"+today.getDate()+"日");
+console.log(year+month+day);    
